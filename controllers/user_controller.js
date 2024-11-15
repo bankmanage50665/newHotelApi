eb20279b5675145559bba90b6e156bd5da6dacd9
bankmanage50665 <rahulkumawatr50666@gmail.com>
2024-11-16 04:51:03 +0530

    Fix otp bug

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -87,11 +87,11 @@ async function sendOTP(req, res, next) {
       );
     }
 
-    // await client.messages.create({
-    //   body: `Otp from Shyam Rooms: Your OTP is: ${otp}`,
-    //   from: process.env.TWILIO_PHONE_NUMBER,
-    //   to: `+91${phoneNumber}`,
-    // });
+    await client.messages.create({
+      body: `Otp from Shyam Rooms: Your OTP is: ${otp}`,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: `+91${phoneNumber}`,
+    });
 
     console.log(otp);
 
